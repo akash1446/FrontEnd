cb3b71a068a95b1eab81c2e886445d57d9c9e030
akash1446 <guduriakash9@gmail.com>
2025-10-30 18:18:48 +0530

    some modifiacation of the  my project file

---
+++ IntroJS/script.js
@@ -0,0 +1,18 @@
+console.log("hello guys welcome to zomato")
+
+console.warn("programming code is warning display")
+console.error("developer your code is error")
+
+alert("save the password")
+
+
+var a; //declaration
+a=20; //intialization
+console.log(a);
+
+var b=10;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
