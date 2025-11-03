c853379e652180834b1124eb986683dc66d4a63f
akash1446 <guduriakash9@gmail.com>
2025-11-03 11:40:30 +0530

    Two or more file will be added

--- IntroJS/script.js
+++ IntroJS/script.js
@@ -6,13 +6,49 @@ console.error("developer your code is error")
 alert("save the password")
 
 
-var a; //declaration
-a=20; //intialization
+var a=10; //declaration
 console.log(a);
+console.log(a++);
+console.log(++a);
+
 
 var b=10;
 console.log(a+b);
-console.log(a-b);
-console.log(a*b);
-console.log(a/b);
-console.log(a%b);
+console.log(b--);
+console.log(--b);
+
+var num1 = prompt("enter number1")
+var num2 = prompt("enter number2")
+console.log(num1 + num2)
+var real1 = Number(num1)
+console.log(real1)
+var real2 = Number(num2)
+console.log(real1-real2)
+
+var value = confirm('How are you my friend')
+console.log(value)
+
+var c = 5;
+var d = 10;
+console.log(c+d)
+console.log('c'+c+'d'+d);
+
+var e=10;
+console.log(e);
+e=9
+console.log(e)
+
+var bride = 'ramudu'
+var groom = 'chai'
+
+bride='seetha'
+
+console.log(bride, 'weds', groom)
+
+var a = null
+console.log(a)
+
+var a = "true"
+console.log(a)
+
+
---
+++ JavaScript/script.js
@@ -0,0 +1,41 @@
+console.log("Hi guys build apis")
+// logical operators
+let transfermoney = true;
+let isnottransforcash = false;
+console.log(transfermoney && isnottransforcash) 
+console.log(transfermoney || isnottransforcash)
+console.log(!transfermoney)
+
+//comparsion operators
+let a = 10;
+let b = 20;
+console.log(a>=b)
+console.log(a<=b)
+console.log(a==b)
+console.log(a===b) //strict comparsion
+console.log(a=b) // Assignment operator
+
+// objects
+let  username = 'Akash';
+username = 'q12eer';
+console.log(username)
+
+let userName = {
+                firstName : 'ravi',
+                age:24,
+                village:'veeravasram'
+               }
+
+console.log(
+            userName.firstName,
+            userName.age,
+            userName.village
+);
+ userName.firstName='i'
+ console.log(userName.firstName)
+
+// Arrays
+let heros = ['a','b','c']
+console.log(heros[0])
+console.log(heros[1])
+console.log(heros[2])
